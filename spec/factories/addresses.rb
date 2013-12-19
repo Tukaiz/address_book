cedc3648a7e8239f8ecfaf877454a92a07ead4a3
Abraham Oshel <abraham.oshel@gmail.com>
2013-12-19 10:16:36 -0600

    update specs for recent validation change

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,13 +1,15 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do
-  factory :address do
-    line_1 { Faker::Address.street_address(include_secondary = false) }
-    line_2 { Faker::Address.street_address(include_secondary = false) }
-    line_3 { Faker::Address.street_address(include_secondary = false) }
-    city { Faker::Address.city }
-    province {  Faker::Address.us_state_abbr }
-    postal_code { Faker::Address.zip_code }
-    country { Faker::Address.country }
-  end
-end
+ FactoryGirl.define do
+   factory :address do
+    factory :valid_address do
+      line_1 { Faker::Address.street_address(include_secondary = false) }
+      line_2 { Faker::Address.street_address(include_secondary = false) }
+      line_3 { Faker::Address.street_address(include_secondary = false) }
+      city { Faker::Address.city }
+      province {  Faker::Address.state_abbr }
+      postal_code { Faker::Address.zip_code }
+      country { Faker::Address.country }
+    end
+   end
+ end
